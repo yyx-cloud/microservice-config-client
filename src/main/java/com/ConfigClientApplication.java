7667afb0d5115f8fa84048a02d1ed4a37ffc33c6
yyx19941129 <18600094425@163.com>
2019-01-14 17:37:53 +0800

    添加Eureka注册发现Config Server

--- src/main/java/com/ConfigClientApplication.java
+++ src/main/java/com/ConfigClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
@@ -13,6 +14,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  */
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableDiscoveryClient
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class);
--- src/main/java/com/controller/ProfileController.java
+++ src/main/java/com/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+//@RefreshScope
 public class ProfileController {
 
     @Value("${profile}")
